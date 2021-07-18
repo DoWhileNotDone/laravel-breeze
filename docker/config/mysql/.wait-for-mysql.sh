800c3708d15a51719ce6a17ec61f9a6de531d0cc
DavegTheMighty <dave@example.com>
2021-07-18 18:42:21 +0100

    Laravel Base Project
     * Add Laravel Dusk
     * Add Selenium Webdriver

--- docker/config/mysql/.wait-for-mysql.sh
+++ docker/config/mysql/.wait-for-mysql.sh
@@ -6,3 +6,10 @@ PASSWORD="demo_pw"
 while ! docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
     sleep 1
 done
+
+CONTAINER="demo-mysql-dusk"
+USERNAME="demo_usr"
+PASSWORD="demo_pw"
+while ! docker exec $CONTAINER mysql --user=$USERNAME --password=$PASSWORD -e "SELECT 1" >/dev/null 2>&1; do
+    sleep 1
+done
